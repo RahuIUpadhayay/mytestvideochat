aa38b6244d7c0c3a50b5a9f4d3f1b202c83b7c3e
rahul upadhayay <rahul5.up55@gmail.com>
2020-06-09 00:58:55 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+let express = require( 'express' );
+let app = express();
+let server = require( 'http' ).Server( app );
+let io = require( 'socket.io' )( server );
+let stream = require( './ws/stream' );
+let path = require( 'path' );
+
+
+
+app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
+
+app.get( '/', ( req, res ) => {
+    res.sendFile( __dirname + '/index.html' );
+} );
+
+const PORT = process.env.PORT || 3000;
+io.of( '/stream' ).on( 'connection', stream );
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
